304915340948db8c8fe7d3248efb6b65fc99ebd4
lindsfitz <lindsaytfitz@gmail.com>
2022-08-17 14:26:30 -0700

    placeholder data for tests and light styling added to dashboard

--- src/App.js
+++ src/App.js
@@ -6,7 +6,26 @@ function App() {
     <div className="App">
       <Navigation />
       <Dashboard />
-     
+
+
+
+
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}>
+            <Route path='home' element={<Home />} />
+            <Route path="search" element={<Search />} />
+            <Route path="books" element={<AllBooks />}>
+              <Route path=":bookId" element={<OneBook />} />
+              <Route path="new" element={<NewBookForm />} />
+            </Route>
+            <Route path="shelves" element={<AllShelves />}>
+              <Route path=":shelfId" element={<Bookshelf />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter> */}
+
     </div>
   );
 }
--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -19,65 +19,167 @@ import CardMedia from '@mui/material/CardMedia';
 import Container from '@mui/material/Container';
 import ImageListItem from '@mui/material/ImageListItem';
 
+
+const tempData = [
+    {
+        name: 'Faves',
+        description: 'All time favorite books',
+        Books: [
+            {
+                title: "House of Earth and Blood",
+                author: "Sarah J. Maas",
+                cover_img: "https://covers.openlibrary.org/b/olid/OL27924119M-M.jpg",
+                pages: 816,
+                edition_key: "OL35866018M"
+            },
+            {
+                title: "House of Sky and Breath",
+                author: "Sarah J. Maas",
+                cover_img: "https://covers.openlibrary.org/b/olid/OL28946291M-M.jpg",
+                pages: 807,
+            },
+            {
+                title: "The Ruin of Kings",
+                author: "Jenn Lyons",
+                cover_img: "https://covers.openlibrary.org/b/olid/OL27760114M-M.jpg",
+                pages: 576,
+                edition_key: "OL29792696M"
+            },
+            {
+                title: "The Memory of Souls",
+                author: "Jenn Lyons",
+                cover_img: "https://covers.openlibrary.org/b/olid/OL28178905M-M.jpg",
+                pages: 656,
+                edition_key: "OL29831091M"
+            },
+            {
+                title: "The Discord of Gods",
+                author: "Jenn Lyons",
+                cover_img: "https://covers.openlibrary.org/b/olid/OL34161957M-M.jpg",
+                pages: 512,
+                edition_key: "OL37998226M"
+            },
+        ]
+
+
+    },
+    {
+        name: "TBR",
+        description: "Desperately want to read these",
+        Books: [
+            {
+                title: "Empire of Storms",
+                author: "Sarah J. Maas",
+                cover_img: "https://covers.openlibrary.org/b/olid/OL26319926M-M.jpg",
+                pages: 712,
+                edition_key: "OL27696715M"
+            },
+            {
+                title: "A Court of Wings and Ruin",
+                author: "Sarah J. Maas",
+                cover_img: "https://covers.openlibrary.org/b/olid/OL26832221M-M.jpg",
+                pages: 720,
+                edition_key: "OL37072070M"
+            },
+            {
+                title: "A Court of Thorns and Roses",
+                author: "Sarah J. Maas",
+                cover_img: "https://covers.openlibrary.org/b/olid/OL27099075M-M.jpg",
+                pages: 432,
+                edition_key: "OL31959292M"
+            },
+            {
+                title: "A Court of Mist and Fury",
+                author: "Sarah J. Maas",
+                cover_img: "https://covers.openlibrary.org/b/olid/OL26992991M-M.jpg",
+                pages: 640,
+                edition_key: "OL32856480M"
+            },
+
+        ]
+    },
+    {
+        name: "Upcoming Releases",
+        description: "Counting Down the Days",
+        Books: [
+            {
+                title: "House of Sky and Breath",
+                author: "Sarah J. Maas",
+                cover_img: "https://covers.openlibrary.org/b/olid/OL28946291M-M.jpg",
+                pages: 807,
+            },
+            {
+                title: "The Discord of Gods",
+                author: "Jenn Lyons",
+                cover_img: "https://covers.openlibrary.org/b/olid/OL34161957M-M.jpg",
+                pages: 512,
+                edition_key: "OL37998226M"
+            },
+
+        ]
+    },
+    {
+        name: "DNF",
+        description: "Boo these books",
+        Books: [
+            {
+                title: "A Touch of Darkness",
+                author: "Scarlett St. Clair",
+                cover_img: "https://covers.openlibrary.org/b/olid/OL28946291M-M.jpg",
+                pages: 354,
+                edition_key: "OL31995429M"
+            },
+        ]
+    },
+]
+
+
 export default function Dashboard({ user }) {
+
+    // use effect to perform api call on page load , change the shelves piece of state post use effect and use the state variable to render the dependent components?
 
     const renderShelves = async () => {
         const userShelves = await API.getShelves(user.id)
-
-        for (let i = 0; i < userShelves.length; i++) {
-
-
-        }
     }
 
     return (
         <React.Fragment>
-
-            {/* <Container maxWidth="md">
-                <Typography sx={{ minWidth: 100 }}>Shelf Name</Typography>
-                <Container maxWidth="md" sx={{ display: 'flex' }}> */}
-            {/* <Card >
-                        <CardMedia
-                            component="img"
-                            height="194"
-                            image="https://covers.openlibrary.org/b/olid/OL27924119M-M.jpg"
-                            alt="Crescent City Cover"
-                        />
-
-                    </Card>
-                    <Card >
-                        <CardMedia
-                            component="img"
-                            height="194"
-                            image="https://covers.openlibrary.org/b/olid/OL27924119M-M.jpg"
-                            alt="Crescent City Cover"
-                        />
-                        <CardContent> */}
-            {/* https://covers.openlibrary.org/b/olid/OL27924119M-M.jpg */}
-            {/* <Typography gutterBottom variant="subtitle1">
-                                House of Earth and Blood
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Sarah J. Maas
-                            </Typography>
-                        </CardContent>
-                    </Card>
-
-                </Container>
-            </Container> */}
-
-
-
-
-
 
 
 
             {/* ONE LIST COMPONENT ALREADY EXISTS PRE DATA PULL */}
             {/* ***** ONCE DATA IS BEING PULLED VIA API SUCCESSFULLY - MAP OVER SHELF RESULTS. CREATE LIST ITEM COMPONENT FOR EACH SHELF IN RESULTS DATA & LIST ITEM TEXT FOR THE TITLE OF EACH SHELF */}
             {/* *** ONE IMAGE LIST ITEM IS CREATED FOR EACH BOOK INSIDE OF THE SHELF. JUST SET THE SRC TO THE IMAGE LINK FROM THE RESULTS */}
-
             <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                {tempData.map((shelf) => (
+                    <React.Fragment>
+                        <ListItem sx={{ flexDirection: 'column', alignItems: 'flex-start' }}>
+                            <ListItemText
+                                primary={`${shelf.name}`}
+                                sx={{ maxWidth: '10%' }}
+                            />
+                            <div style={{ display: 'flex', width: '100%' }}>
+
+                                {shelf.Books.map((book) => (
+                                    <ImageListItem key={book.title} sx={{ maxHeight: 218, maxWidth: 148, margin: 2 }}>
+                                        <img
+                                            src={`${book.cover_img}`}
+                                            srcSet={`${book.cover_img}`}
+                                            alt={`${book.title}`}
+                                            loading="lazy"
+
+                                        />
+                                    </ImageListItem>
+
+                                ))}
+                            </div>
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                    </React.Fragment>
+                ))}
+            </List>
+
+            {/* <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
                 <ListItem alignItems="flex-start">
                     <ListItemText
                         primary="Shelf Title"
@@ -152,7 +254,49 @@ export default function Dashboard({ user }) {
                         }
                     />
                 </ListItem>
-            </List>
+            </List> */}
+
+
+
+
+
+            {/* <Container maxWidth="md">
+                <Typography sx={{ minWidth: 100 }}>Shelf Name</Typography>
+                <Container maxWidth="md" sx={{ display: 'flex' }}> */}
+            {/* <Card >
+                        <CardMedia
+                            component="img"
+                            height="194"
+                            image="https://covers.openlibrary.org/b/olid/OL27924119M-M.jpg"
+                            alt="Crescent City Cover"
+                        />
+
+                    </Card>
+                    <Card >
+                        <CardMedia
+                            component="img"
+                            height="194"
+                            image="https://covers.openlibrary.org/b/olid/OL27924119M-M.jpg"
+                            alt="Crescent City Cover"
+                        />
+                        <CardContent> */}
+            {/* https://covers.openlibrary.org/b/olid/OL27924119M-M.jpg */}
+            {/* <Typography gutterBottom variant="subtitle1">
+                                House of Earth and Blood
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Sarah J. Maas
+                            </Typography>
+                        </CardContent>
+                    </Card>
+
+                </Container>
+            </Container> */}
+
+
+
+
+
         </React.Fragment>
     )
 
