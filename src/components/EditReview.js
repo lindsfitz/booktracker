479ab60a99cc1763a232bb78498a14ae9bb36ddc
lindsfitz <lindsaytfitz@gmail.com>
2022-09-13 17:24:55 -0700

    can add/edit/delete reviews

---
+++ src/components/EditReview.js
@@ -0,0 +1,179 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import AppContext from '../AppContext';
+import API from '../utils/API'
+import { Typography, FormControl, Rating, Stack, Switch, Box, TextField, Button } from '@mui/material/';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+
+
+export default function EditReview({reviewData, setEditReview, reviewInfo, setEditId}) {
+
+    const context = useContext(AppContext);
+    const params = useParams();
+
+    const [startValue, setStartValue] = useState(null);
+    const [endValue, setEndValue] = useState(null);
+    const [readSwitch, setReadSwitch] = useState(true)
+
+    useEffect(()=>{
+        setStartValue(reviewData.date_started)
+        setEndValue(reviewData.date_finished)
+        setReadSwitch(reviewData.read)
+    },[])
+
+    const handleSwitch = (event) => {
+        setReadSwitch(event.target.checked);
+    };
+
+
+    const handleStartDate = (newValue) => {
+        setStartValue(newValue);
+    };
+
+    const handleEndDate = (newValue) => {
+        setEndValue(newValue);
+    };
+
+    const reviewSubmit = async (e) => {
+        e.preventDefault();
+        console.log('submitted')
+        let updatedReview;
+        const data = new FormData(e.currentTarget)
+        let startDate = dayjs(startValue)
+        let finishDate = dayjs(endValue)
+        if (readSwitch) {
+            updatedReview = {
+                read: readSwitch,
+                date_started: startDate.format('YYYY/MM/DD'),
+                date_finished: finishDate.format('YYYY/MM/DD'),
+                year_finished: finishDate.year(),
+                month_finished: finishDate.month(),
+                rating: data.get('rating'),
+                review: data.get('review'),
+                format: data.get('format'),
+                series: data.get('series'),
+            }
+        }
+        if (!readSwitch) {
+            updatedReview = {
+                read: readSwitch,
+                review: data.get('review'),
+            }
+        }
+        console.log(updatedReview)
+        const updated = await API.editReview(updatedReview,reviewData.id)
+        console.log(updated)
+        reviewInfo()
+        setEditId(null)
+        setEditReview(false)
+    }
+
+    return (
+
+        <React.Fragment>
+            <Box
+                component="form"
+                sx={{ m: 1, width: '50%' }}
+                noValidate
+                autoComplete="off"
+                onSubmit={reviewSubmit}
+            >
+
+                <Stack direction="row" spacing={4} alignItems="center" justifyContent="center">
+
+
+                    <Stack spacing={3} alignItems="center" justifyContent="center">
+
+                        {/* For the review form I need:
+                    - Toggle for True/False if they are marking the book as read or unread */}
+
+                        <Stack direction="row" spacing={1} alignItems="center">
+                            <Typography>Unread</Typography>
+                            <Switch name='read'
+                                id='read'
+                                checked={readSwitch}
+                                onChange={handleSwitch}
+                                inputProps={{ 'aria-label': 'controlled' }} />
+                            <Typography>Read</Typography>
+                        </Stack>
+
+                        {readSwitch && <Stack spacing={3}>
+                            {/* - Date picker for the start date */}
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+
+                                <DesktopDatePicker
+                                    label="Date Started"
+                                    name='started'
+                                    id='started'
+                                    // inputFormat="MM/dd/yyyy"
+                                    value={startValue}
+                                    onChange={handleStartDate}
+                                    renderInput={(params) => <TextField {...params} />}
+                                />
+                                {/* - Date picker for date finished */}
+                                <DesktopDatePicker
+                                    label="Date Finished"
+                                    id='finished'
+                                    name='finished'
+                                    value={endValue}
+                                    onChange={handleEndDate}
+                                    renderInput={(params) => <TextField {...params} />}
+                                />
+                            </LocalizationProvider>
+                        </Stack>
+                        }
+                    </Stack>
+
+                    {readSwitch && <Stack spacing={3} >
+                        <Stack direction="row" spacing={0}>
+                            <Typography component="legend">Your Rating:</Typography>
+                            <Rating name="rating" id='rating' defaultValue={reviewData.rating} precision={0.5} />
+                        </Stack>
+
+                        {/* - Dropdown menu for the format 
+                    - Ebook, hard copy */}
+                        {/* - Or maybe a text field for format, not sure yet */}
+                        <TextField
+                            name='format'
+                            id="format"
+                            label="Format"
+                            defaultValue={reviewData.format}
+                        />
+                        {/* - Textfield for Series? */}
+                        <TextField
+                            name='series'
+                            id="series"
+                            label="Series Details"
+                            defaultValue={reviewData.series}
+                        />
+                    </Stack>}
+
+                </Stack>
+
+
+
+                {/* - Textbox for the actual review */}
+                <FormControl fullWidth sx={{ m: 1 }}>
+
+                    <TextField
+                        id="review"
+                        name='review'
+                        label={readSwitch ? 'Review' : 'Notes'}
+                        multiline
+                        rows={10}
+                        defaultValue={reviewData.review}
+
+                    />
+                </FormControl>
+
+                <Button type='submit'>Update Review</Button>
+
+            </Box>
+
+        </React.Fragment>
+    )
+}
--- src/pages/Books/UserBook.js
+++ src/pages/Books/UserBook.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect, useContext } from 'react';
 import { useParams } from "react-router-dom";
 import AppContext from '../../AppContext';
 import API from '../../utils/API'
-import { Card, Button, Rating, CardContent, CardMedia, Typography, Box, Paper, Divider } from '@mui/material';
+import { Card, Button, Rating, CardContent, CardMedia, Typography, Box, Container, Paper, Divider, Switch, Stack, Chip, Link, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 import AddReview from './AddReview';
+import EditReview from '../../components/EditReview';
 
 
 // single book page - top section of the page has lots of details about the book (maybe one template for super detailed results from open library, one template for user added books w a bit less detail included)
@@ -17,8 +20,10 @@ export default function UserBook() {
     const params = useParams();
 
     const [bookData, setBookData] = useState(null)
-    const [reviewData, setReviewData] = useState([])
+    const [reviewData, setReviewData] = useState(false)
     const [reviewForm, setReviewForm] = useState(false)
+    const [editReview, setEditReview] = useState(false);
+    const [editId, setEditId] = useState(null)
 
 
     const bookInfo = async () => {
@@ -27,16 +32,27 @@ export default function UserBook() {
         setBookData(book.data)
         console.log(bookData)
     }
-    
+
     const reviewInfo = async () => {
         const review = await API.getOneReview(context.userData.id, params.id)
-        // console.log(review)
-        setReviewData(review.data)
+        console.log(review)
+        if (review.data.length) {
+            setReviewData(review.data)
+        }
 
     }
 
     const toggleReviewForm = () => {
         setReviewForm(!reviewForm)
+    }
+
+    const toggleEditForm = (id) => {
+        setEditId(id)
+        setEditReview(!editReview)
+    }
+
+    const deleteReview = async (id) => {
+        API.deleteReview(id)
     }
 
     useEffect(() => {
@@ -47,66 +63,133 @@ export default function UserBook() {
 
     return (
         <React.Fragment>
-            {bookData && reviewData && <div>
-                <Card>
-                    <CardMedia
-                        component="img"
-                        height="194"
-                        image={`${bookData.cover_img}`}
-                        alt={`${bookData.title}`}
-                    />
-                    {/* <img
+            {bookData && <div>
+                <Container sx={{ display: "flex", m: 3, p: 2 }}>
+                    <Card sx={{ maxWidth: 345 }} >
+                        <CardContent>
+                            <CardMedia
+                                component="img"
+                                // height="194"
+                                image={`${bookData.cover_img}`}
+                                alt={`${bookData.title}`}
+                            />
+                            {/* <img
                         src={`${bookData.cover_img}`}
                         srcSet={`${bookData.cover_img}`}
                         alt={`${bookData.title}`}
                         loading="lazy"
                     /> */}
-                    <CardContent>
+                        </CardContent>
+                    </Card>
+                    <Box sx={{ maxWidth: "60%", p: 4 }}>
                         <Typography gutterBottom variant="h5" component="div">
                             {bookData.title}
                         </Typography>
-                        <Typography variant="subtitle1" color="text.secondary">
-                            {bookData.author}
+                        <Typography variant="subtitle2" color="text.secondary">
+                            by {bookData.author}
                         </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {bookData.pages}
+                        <Divider />
+                        <Typography variant="caption" color="text.secondary">
+                            Pages: {bookData.pages}
                         </Typography>
-                    </CardContent>
-                </Card>
+                        <Typography variant="caption" color="text.secondary">
+                            Published: {bookData.published}
+                        </Typography>
+                        <Divider />
+                        <Typography variant='body2'>
+                            {bookData.description}
+                        </Typography>
+
+                    </Box>
+
+                </Container>
+
+
+
                 <Divider />
                 {!reviewForm && <div>
                     <Button onClick={toggleReviewForm}>Add A New Review</Button>
                 </div>}
                 {reviewForm && <div>
-                    <AddReview reviewInfo={reviewInfo} toggleReviewForm={toggleReviewForm}/>
+                    <AddReview reviewInfo={reviewInfo} toggleReviewForm={toggleReviewForm} />
                     <Button onClick={toggleReviewForm}>Cancel</Button>
                 </div>
                 }
                 <Divider />
-                {reviewData.map((review) => (
-                    <Paper key={`${review.id}`} elevation={3}>
-                        <Box>
-                            <Typography variant="body2" color="text.secondary">
-                                {review.date_started}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                {review.date_finished}
-                            </Typography>
-                            <Rating name="half-rating-read" defaultValue={review.rating} precision={0.5} readOnly />
-                            <Typography variant="subtitle1" color="text.secondary">
-                                {review.review}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                {review.format}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                {review.series}
-                            </Typography>
-                        </Box>
-                    </Paper>
-                ))}
+
+                <Container>
+
+                    {!reviewData && !reviewForm && <div style={{ margin: '30px auto 0 auto', textAlign: 'center' }}>
+
+                        <Typography variant='subtitle2'>
+                            It looks like you haven't reviewed this book yet.
+                        </Typography>
+
+                        <Link variant='subtitle2' onClick={toggleReviewForm}>Add Your Thoughts</Link>
+                    </div>}
+
+
+                    {reviewData && <Container>
+
+                        <Typography variant='subtitle1'>
+                            Your Reviews:
+                        </Typography>
+
+
+                        {reviewData.map((review) => (
+                            <Paper key={`${review.id}`} elevation={6} sx={{ width: '60%', p: 2 }}>
+                                {editId !== review.id && <Container>
+                                    <Stack spacing={0.5} direction="row" justifyContent="flex-end">
+                                        <IconButton onClick={() => toggleEditForm(review.id)} aria-label="delete" size="small">
+                                            <EditIcon fontSize="inherit" />
+                                        </IconButton>
+                                        <IconButton onClick={() => deleteReview(review.id)} aria-label="delete" size="small">
+                                            <DeleteIcon fontSize="inherit" />
+                                        </IconButton>
+                                    </Stack>
+                                    <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                        <Stack spacing={3} sx={{ m: 1, p: 1 }}>
+                                            <Stack direction="row" spacing={1} alignItems="center">
+                                                <Typography>Unread</Typography>
+                                                <Switch name='read'
+                                                    id='read'
+                                                    checked={review.read}
+                                                    disabled />
+                                                <Typography>Read</Typography>
+                                            </Stack>
+
+                                            <Chip label={`Date Started: ${review.date_started}`} variant='outlined' />
+
+                                            <Chip label={`Date Finished: ${review.date_finished}`} variant='outlined' />
+                                        </Stack>
+
+
+                                        <Stack spacing={3} sx={{ m: 1, p: 1 }}>
+                                            <Stack direction="row" spacing={0}>
+                                                <Typography component="legend">Your Rating:</Typography>
+                                                <Rating name="half-rating-read" defaultValue={review.rating} precision={0.5} readOnly />
+                                            </Stack>
+                                            <Chip label={`Format: ${review.format}`} variant='outlined' />
+
+                                            <Chip label={`Series: ${review.series}`} variant='outlined' />
+                                        </Stack>
+                                    </Box>
+
+                                    <Typography variant="subtitle1" color="text.secondary">
+                                        {review.review}
+                                    </Typography>
+                                </Container>}
+                                {editId===review.id &&
+                                    <EditReview reviewData={review} setEditReview={setEditReview} reviewInfo={reviewInfo} setEditId={setEditId} />
+                                 }
+                            </Paper>
+                        ))}
+                    </Container>}
+                </Container>
 
             </div>}
+
+            {/* {editReview && } */}
 
         </React.Fragment>
     )
